5cad89ebc8bd25a0cfeb3fd688dbc88c6b48fbd2
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-02-25 17:43:24 +0000

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import Hero from './components/Hero'
+import Index from './components/Index'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-   <Hero/>
+   <Index/>
     </>
   )
 }
